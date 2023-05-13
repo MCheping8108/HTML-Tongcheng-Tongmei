08195b75c3e4ee41d205e69b4a0e732f58937628
和平peaceful <102772286+mcheping520@users.noreply.github.com>
2023-05-13 09:15:13 +0000

    2023-5-13-3

--- 76/js/diary.js
+++ 76/js/diary.js
@@ -8,11 +8,25 @@ publish.onclick = function() {
 function f() {
 	var diary = document.createElement("div");
 	var p = document.createElement("p");
-	
+	var time = document.createElement("span");
+	var date = new Date();
+	time.innerHTML = getTime();
+	diary.appendChild(time);
 	p.innerHTML = txt.value;
 	show.appendChild(diary);
 	diary.appendChild(p);
 	txt.value = "";
+}
+
+function getTime() {
+	var myDate = new Date();
+	var nowMonth = myDate.getMonth();
+	var nowDay = myDate.getDay();
+	var nowHours = myDate.getHours();
+	var nowMin = myDate.getMinutes();
+	
+	var nowTime = nowMonth+1+"月"+nowDay+"日"+nowHours+":"+nowMin;
+	return nowTime;
 }
 
 
